cd35e188d237bf7e68408e5ee9373ea73490f22b
Rodrigo-ACR <rodrigoantonioconcharomero@gmail.com>
2026-09-17 21:28:56 -0300

    Fix secure context headers for MSAL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -13,5 +12,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    https: false,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    }
   },
 })
