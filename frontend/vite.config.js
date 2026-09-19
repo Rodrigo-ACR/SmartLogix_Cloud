b764a94b044170af495b80962d101e3054355669
Rodrigo-ACR <rodrigoantonioconcharomero@gmail.com>
2026-09-18 21:03:13 -0300

    Remove COOP/COEP headers blocking Cloudinary images

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,10 +12,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    https: false,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    }
   },
 })
